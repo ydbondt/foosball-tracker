b83ff7d98db55d38dabf7a171d67a840b6e696c8
ccantill <christoph.cantillon@gmail.com>
2018-12-07 14:06:57 +0100

    more performance

--- detect-ball.py
+++ detect-ball.py
@@ -21,30 +21,68 @@ lower_orange = np.array([0, 162, 186])
 upper_orange = np.array([46, 242, 255])
 
 prev_point = None
+next_point = None
+
+subframe_size = 200
+
+history_points = []
+
+def processFrame(w_img, prev_point):
+    frame = cv2.UMat(w_img)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    rangeMask = cv2.inRange(hsv, lower_orange, upper_orange)
+    image, contours, hierarchy = cv2.findContours(rangeMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+    for c in contours:
+        M = cv2.moments(c)
+        perimeter = cv2.arcLength(c, True)
+        if M["m00"] != 0 and perimeter > 100:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            point = (cX, cY)
+            return point
+    return None
+
+
+def drawHistoryPoints(img, history_points):
+    pp = None
+    for p in history_points:
+        if pp is not None and p is not None:
+            cv2.line(img, pp, p, (0, 255, 0), 2)
+        pp = p
+
 
 while (cap.isOpened() == True):
     ret, img = cap.read()
     if ret == True:
-        frame = cv2.UMat(img)
-        
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        origin = None
 
-        rangeMask = cv2.inRange(hsv, lower_orange, upper_orange)
+        if next_point is not None:
+            origin = (max(0, next_point[0] - (subframe_size >> 1)), max(0, next_point[1] - (subframe_size >> 1)))
+            w_img = img[origin[1]:min(len(img), origin[1] + subframe_size), origin[0]:min(len(img[0]), origin[0] + subframe_size)]
+            point = processFrame(w_img, prev_point)
+            # cv2.imshow('Subframe', w_img)
+            if point is None:
+                point = processFrame(img, prev_point)
+            else:
+                point = (point[0] + origin[0], point[1] + origin[1])
+        else:
+            origin = (0,0)
+            point = processFrame(img, prev_point)
 
-        image, contours, hierarchy = cv2.findContours(rangeMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+        if point is None:
+            prev_point = None
+            next_point = None
+        else:
+            if prev_point is not None:
+                cv2.line(img, prev_point, point, (0, 255, 0), 2)
+                next_point = (point[0] * 2 - prev_point[0], point[1] * 2 - prev_point[1])
+                if next_point[0] < 0 or next_point[0] > len(img[0]) or next_point[1] < 0 or next_point[1] > len(img):
+                    next_point = point
+            prev_point = point
 
-        for c in contours:
-            M = cv2.moments(c)
-            perimeter = cv2.arcLength(c, True)
-            if M["m00"] != 0 and perimeter > 100:
-                cv2.drawContours(img, [c], -1, (0,255,0), 3)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                point = (cX, cY)
-                if (prev_point is not None):
-                    cv2.line(img, prev_point, point, (0,255,0), 2)
-                    prev_point = point
-                break
+        history_points.append(point)
+
+        drawHistoryPoints(img, history_points)
 
         # out.write(img)
         cv2.imshow('Frame', img);
