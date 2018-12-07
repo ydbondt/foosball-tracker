af4dd92c7a9d1a3723856b17005a8c6842ed3d6c
ccantill <christoph.cantillon@gmail.com>
2018-12-07 14:50:05 +0100

    add threading, but somehow it's slower :|

--- detect-ball.py
+++ detect-ball.py
@@ -1,6 +1,7 @@
+import threading
+from queue import Queue
 import cv2
 import numpy as np
-import logging
 
 if cv2.ocl.haveOpenCL():
     cv2.ocl.setUseOpenCL(True)
@@ -20,84 +21,122 @@ ret, first_frame = cap.read()
 lower_orange = np.array([0, 162, 186])
 upper_orange = np.array([46, 242, 255])
 
-prev_point = None
-next_point = None
-
 subframe_size = 200
 
-history_points = []
 
-def processFrame(w_img, prev_point):
-    frame = cv2.UMat(w_img)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    rangeMask = cv2.inRange(hsv, lower_orange, upper_orange)
-    image, contours, hierarchy = cv2.findContours(rangeMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
-    for c in contours:
-        M = cv2.moments(c)
-        perimeter = cv2.arcLength(c, True)
-        if M["m00"] != 0 and perimeter > 100:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            point = (cX, cY)
-            return point
-    return None
+class ImageProcessor:
+    num_frames = 0
+    num_optim_frames = 0
+    next_point = None
+    prev_point = None
+    history_points = []
 
+    def processSubFrame(self, w_img):
+        frame = cv2.UMat(w_img)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        rangeMask = cv2.inRange(hsv, lower_orange, upper_orange)
+        image, contours, hierarchy = cv2.findContours(rangeMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+        for c in contours:
+            M = cv2.moments(c)
+            perimeter = cv2.arcLength(c, True)
+            if M["m00"] != 0 and perimeter > 100:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                point = (cX, cY)
+                return point
+        return None
 
-def drawHistoryPoints(img, history_points):
-    pp = None
-    for p in history_points:
-        if pp is not None and p is not None:
-            cv2.line(img, pp, p, (0, 255, 0), 2)
-        pp = p
+    def drawHistoryPoints(self, img, history_points):
+        pp = None
+        for p in self.history_points:
+            if pp is not None and p is not None:
+                cv2.line(img, pp, p, (0, 255, 0), 2)
+            pp = p
 
-num_frames = 0
-num_optim_frames = 0
+    def processFrame(self, img):
+        self.num_frames += 1
 
-while (cap.isOpened() == True):
-    ret, img = cap.read()
-    if ret == True:
-        origin = None
-
-        num_frames += 1
-
-        if next_point is not None:
-            origin = (max(0, next_point[0] - (subframe_size >> 1)), max(0, next_point[1] - (subframe_size >> 1)))
-            w_img = img[origin[1]:min(len(img), origin[1] + subframe_size), origin[0]:min(len(img[0]), origin[0] + subframe_size)]
-            point = processFrame(w_img, prev_point)
+        if self.next_point is not None:
+            origin = (max(0, self.next_point[0] - (subframe_size >> 1)), max(0, self.next_point[1] - (subframe_size >> 1)))
+            w_img = img[origin[1]:min(len(img), origin[1] + subframe_size),
+                    origin[0]:min(len(img[0]), origin[0] + subframe_size)]
+            point = self.processSubFrame(w_img)
             # cv2.imshow('Subframe', w_img)
             if point is None:
-                point = processFrame(img, prev_point)
+                point = self.processSubFrame(img)
             else:
-                num_optim_frames += 1
+                self.num_optim_frames += 1
                 point = (point[0] + origin[0], point[1] + origin[1])
         else:
-            origin = (0,0)
-            point = processFrame(img, prev_point)
+            point = self.processSubFrame(img)
 
         if point is None:
-            prev_point = None
-            next_point = None
+            self.prev_point = None
+            self.next_point = None
         else:
-            if prev_point is not None:
-                next_point = (point[0] * 2 - prev_point[0], point[1] * 2 - prev_point[1])
-                if next_point[0] < 0 or next_point[0] > len(img[0]) or next_point[1] < 0 or next_point[1] > len(img):
-                    next_point = point
-            prev_point = point
+            if self.prev_point is not None:
+                self.next_point = (point[0] * 2 - self.prev_point[0], point[1] * 2 - self.prev_point[1])
+                if self.next_point[0] < 0 or self.next_point[0] > len(img[0]) or self.next_point[1] < 0 or self.next_point[1] > len(img):
+                    self.next_point = point
+            self.prev_point = point
 
-        history_points.append(point)
+        self.history_points.append(point)
 
-        drawHistoryPoints(img, history_points)
+        # self.drawHistoryPoints(img, history_points)
+        #
+        # cv2.imshow('Frame', img)
+        #
+        # cv2.waitKey(1)
 
-        # out.write(img)
-        cv2.imshow('Frame', img);
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
+class ImageGrabberThread(threading.Thread):
+    def __init__(self, cap, queue):
+        threading.Thread.__init__(self)
+        self.cap = cap
+        self.queue = queue
+
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read();
+            if ret != True:
+                break
+            self.queue.put(frame)
+
+
+class ImageProcessorThread(threading.Thread):
+    def __init__(self, processor, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.processor = processor
+        self.running = True
+
+    def run(self):
+        while self.running:
+            if not self.queue.empty():
+                self.processor.processFrame(self.queue.get())
+
+    def close(self):
+        self.running = False
+
+
+processor = ImageProcessor()
+
+single_thread = True
+
+if single_thread:
+    while cap.isOpened():
+        ret, img = cap.read();
+        if ret == True:
+            processor.processFrame(img)
+        else:
             break
-    else:
-        break
+else:
+    frames = Queue(10)
+    grabber = ImageGrabberThread(cap, frames)
+    main = ImageProcessorThread(processor, frames)
+    grabber.start();
+    main.start();
+    grabber.join();
+    main.close();
 
-cap.release()
-# out.release()
-cv2.destroyAllWindows()
-
-print("Processed ", + num_frames, " of which ", num_optim_frames, " were adequately predicted")
+print("Processed ", + processor.num_frames, " of which ", processor.num_optim_frames, " were adequately predicted")
